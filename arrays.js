f99638f9b7a2f3112eec3e4bac35b502f025e682
punctual-sailor-2214 <bchett286@yahoo.com>
2019-01-04 17:38:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -46,6 +46,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1);
+  array.slice(0, array.length - 1, array.length - 2);
   return array;
 }
