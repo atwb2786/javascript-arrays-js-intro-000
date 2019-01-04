e983b1f0a42ff23c4e95ef450e230d5567a84355
punctual-sailor-2214 <bchett286@yahoo.com>
2019-01-04 16:21:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,6 +12,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  chocolateBars.unshift(element);
-  return chocolateBars;
+  array.unshift(element);
+  return array;
 }
